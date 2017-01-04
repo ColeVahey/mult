b3bcdd9e444fde9fea3b345a5a48ffc14076a8e8
Cole Vahey <rcvahey01@gmail.com>
2017-01-04 18:59:32 -0500

    save

--- .mult/rmold.sh
+++ .mult/rmold.sh
@@ -3,5 +3,5 @@
 '''At the end of development, change this from multtest to mult'''
 '''Will find the mult location and delete the initial repository'''
 cd
-multdirectory="$(find ~ -name multtest)"
+multdirectory="$(find / -name multtest)"
 rm -rf $multdirectory
