9a68fa416d5d86cb3d9e8e505e8f81c829fa1018
Cole Vahey <rcvahey01@gmail.com>
2017-01-04 20:01:09 -0500

    save

--- .mult/rmold.sh
+++ .mult/rmold.sh
@@ -4,7 +4,26 @@
 #Will find the mult location and delete the initial repository
 #NOTE: THIS PROGRAM WILL RUN UNTIL BOTH USERS HAVE THE MULT PROGRAM
 
-cd
-multdirectory="$(find / -name multtest)"
-echo $multdirectory
-rm -rf $multdirectory
+multdirectories="$(find / -name multtest)"
+directory1="$(echo $multdirectories | awk '{print $1}')"
+directory2="$(echo $multdirectories | awk '{print $2}')"
+
+if echo $USER in $directory1 && echo $USER in $directory2
+then echo "USER IS IN THE BOTH DIRECTORIES"
+  echo AN ERROR HAS OCCURRED
+else
+  if echo $USER in $directory2
+  then echo "USER IS IN THE SECOND DIRECTORY"
+    user2=$directory1
+  else
+    if echo $USER in $directory1
+    then echo "USER IS IN THE FIRST DIRECTORY"
+    else
+      echo "USER IS NOT IN EITHER DIRECTORY... RMOLD PREVIOUSLY RUN"
+    fi
+  fi
+fi
+
+echo $multdirectories
+echo $user2
+#rm -rf $multdirectory
