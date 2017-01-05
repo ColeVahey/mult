4df45caa62610f54632f56a8a0ded186b0d2e278
Cole Vahey <rcvahey01@gmail.com>
2017-01-05 09:13:57 -0500

    save

--- .mult/rmold.sh
+++ .mult/rmold.sh
@@ -5,27 +5,6 @@
 #NOTE: THIS PROGRAM WILL RUN UNTIL BOTH USERS HAVE THE MULT PROGRAM
 #THE "IN" IS MESSED UP... KEEP TESTING
 
-multdirectories="$(find / -name multtest)"
-directory1="$(echo $multdirectories | awk '{print $1}')"
-directory2="$(echo $multdirectories | awk '{print $2}')"
-
-if echo $USER in $directory1 && echo $USER in $directory2
-then echo "USER IS IN THE BOTH DIRECTORIES"
-  echo AN ERROR HAS OCCURRED
-else
-  if echo $USER in $directory2
-  then echo "USER IS IN THE SECOND DIRECTORY"
-    user2=$directory1
-  else
-    if echo $USER in $directory1
-    then echo "USER IS IN THE FIRST DIRECTORY"
-      user2=$directory2
-    else
-      echo "USER IS NOT IN EITHER DIRECTORY... RMOLD PREVIOUSLY RUN"
-    fi
-  fi
-fi
-
-echo $multdirectories
-echo $user2
+multdirectory="$(cat .multlocation)"
+echo $multdirectory
 #rm -rf $multdirectory
